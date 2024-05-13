fb89fd08be6dce7aa05c6dd9dd70785842391d19
David Maranto <D.Maranto1122@gmail.com>
2024-05-13 13:09:46 -0400

    Reduce to hello world

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from renogymodbus import RenogyChargeController
+# from renogymodbus import RenogyChargeController
 import time
 
 while True:
     try:
-        controller = RenogyChargeController("/dev/ttyUSB0", 1)
-        print(controller.get_solar_voltage())
+        # controller = RenogyChargeController("/dev/ttyUSB0", 1)
+        # print(controller.get_solar_voltage())
+        print("Hello world!")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        # print(f"An error occurred: {e}")
         time.sleep(10)
