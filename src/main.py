c4a9bf5cef39e77d24a0afc8337d9faa17c4bd03
David Maranto <D.Maranto1122@gmail.com>
2024-05-13 12:46:37 -0400

    Add while true loop

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from renogymodbus import RenogyChargeController
+import time
 
-controller = RenogyChargeController("/dev/ttyUSB0", 1)
-
-print(controller.get_solar_voltage())
+while True:
+    try:
+        controller = RenogyChargeController("/dev/ttyUSB0", 1)
+        print(controller.get_solar_voltage())
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        time.sleep(10)
